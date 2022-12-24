8e033e74738a58c020d6c0b5a25c950c57a8f558
Aki-Miyazaki <miyazaki.ak23@gmail.com>
2022-12-24 14:20:43 +0900

    greet関数を追加

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 def hello():
     return "Hello World"
 
+@app.route('/greet')
+def greet():
+    return "こんにちは"
+
 if __name__ == "__main__":
     # フラスクが持っているアプリを実行します
     app.run(port=8888,debug=True)
